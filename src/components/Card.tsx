bae1326e79ae307b719025a2c6310cc6a58f9432
Dini Putri <mahardini.rizky@gmail.com>
2024-03-22 05:18:36 +1100

    Repo setup

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface CardProps {}
+
+const Card: React.FC<CardProps> = () => {
+  return (
+    <>
+      <a href="">
+        <h1>
+          <div>Test</div>
+        </h1>
+      </a>
+    </>
+  )
+}
+
+export default Card
