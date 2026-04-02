4c46fc4ac52631f08e723d1d88a45077c366fc07
Dini Putri <mahardini.rizky@gmail.com>
2026-04-03 01:57:20 +0900

    📦 refactor: organise into nuxt 4 component layers

---
+++ apps/web/app/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen w-full m-auto">
+    <slot />
+  </div>
+</template>
---
+++ apps/web/app/pages/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { SunMoon } from '@lucide/vue'
+import { Button, ToggleGroup, ToggleGroupItem } from '@portfolio/ui-vue'
+
+const lang = ref<string | null>('en')
+const langModelValue = computed({
+  get: () => lang.value,
+  set: (val) => {
+    if (val === undefined || val === null)
+      return
+    lang.value = val
+  },
+})
+const theme = ref<'light' | 'dark'>('light')
+watch(theme, (newTheme) => {
+  document.body.setAttribute('class', newTheme)
+})
+// Toggle function
+function toggleTheme() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <div class="m-auto flex flex-col items-center gap-2">
+    <Button variant="outline">
+      {{ lang === 'jp' ? 'ハロー、ワールド！' : 'Hello, world!' }}
+    </Button>
+    <ToggleGroup
+      v-model="langModelValue"
+      variant="outline"
+    >
+      <ToggleGroupItem value="en">
+        EN
+      </ToggleGroupItem>
+      <ToggleGroupItem value="jp">
+        JP
+      </ToggleGroupItem>
+    </ToggleGroup>
+    <Button variant="outline" @click="toggleTheme">
+      <SunMoon />
+    </Button>
+    <Button class="font-mono">
+      return 0;
+    </Button>
+  </div>
+</template>
